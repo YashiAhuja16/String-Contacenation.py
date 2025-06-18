ae6f097c19b2c00aadf9825dd1d847baa978a948
YashiAhuja16 <yashi.ahuja16@gmail.com>
2025-06-17 19:21:00 -0700

    Add files via upload

---
+++ String Concentantion and Formatting.py
@@ -0,0 +1,9 @@
+### STRING CONCATENATION AND FORMATTING
+name = "Yashi"
+age = 13 
+print (name, "is", age, "years old")
+# format
+print ("{} is {} years old".format (name,age))
+print (f"{name} is {age} years old.")
+# concatenation 
+print (name + " is " + str(age) + " years old")
